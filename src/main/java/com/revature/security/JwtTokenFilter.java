301b44adb059bb4bd9eb8666b21f53f2d793712d
QuintonCook <Quintoncook97@hotmail.com>
2019-05-04 11:11:20 -0500

    made a successful doFilter test

--- src/main/java/com/revature/security/JwtTokenFilter.java
+++ src/main/java/com/revature/security/JwtTokenFilter.java
@@ -7,15 +7,39 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.revature.exception.CustomException;
 
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	private JwtTokenProvider jwtTokenProvider;
 
+	public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			FilterChain filterChain) throws ServletException, IOException {
+		
+		String token = jwtTokenProvider.resolveToken(httpServletRequest);
+		try {
+			if (token != null && jwtTokenProvider.validateToken(token)) {
+				Authentication auth = jwtTokenProvider.getAuthentication(token);
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		} catch (CustomException ex) {
+			// this is very important, since it guarantees the user is not authenticated at
+			// all
+			SecurityContextHolder.clearContext();
+			httpServletResponse.sendError(ex.getHttpStatus().value(), ex.getMessage());
+			return;
+		}
+
+		filterChain.doFilter(httpServletRequest, httpServletResponse);
 	}
 
 }
--- src/main/java/com/revature/security/JwtTokenFilterConfigurer.java
+++ src/main/java/com/revature/security/JwtTokenFilterConfigurer.java
@@ -3,13 +3,20 @@ package com.revature.security;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class JwtTokenFilterConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
-	private JwtTokenProvider jwtTokenFilterConfigurer;
+	private JwtTokenProvider jwtTokenProvider;
 
-	public JwtTokenFilterConfigurer(JwtTokenProvider jwtTokenFilterConfigurer) {
-		this.jwtTokenFilterConfigurer = jwtTokenFilterConfigurer;
+	public JwtTokenFilterConfigurer(JwtTokenProvider jwtTokenProvider) {
+		this.jwtTokenProvider = jwtTokenProvider;
 	}
-	
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		JwtTokenFilter customFilter = new JwtTokenFilter(jwtTokenProvider);
+		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
 }
